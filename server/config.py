5433086f03bf09d186318c266ff42fe4c860db0c
ewang <ewang@ewang-350V5C.(none)>
2014-10-04 23:38:03 -0400

    Defined database model

--- server/config.py
+++ server/config.py
@@ -1,2 +1,4 @@
 # Flask config
 SQLALCHEMY_DATABASE_URI = "postgresql://newsie@localhost/newsiedb"
+SECRET_KEY = "newstastic"
+
--- server/newsiedb.py
+++ server/newsiedb.py
@@ -11,19 +11,25 @@ class Article(Base):
     # define columns for article table
     id = Column(Integer, primary_key=True)
     url = Column(String(500), nullable=False)
-    sent_pos = Column(Float, nullable=True)
+    sent_pos = Column(Real, nullable=True)
     sent_neg = Column(Float, nullable=True)
 
-    topics = relationship(
-        "Topic",
-        secondary = Table("article_topic", Base.metadata,
-            Column("article_id", Integer, ForeignKey("article.id"), 
-                primary_key = True),
-            Column("topic_id", Integer, ForeginKey("topic.id"),
-                primary_key = True)
-        ),
-    backref = "articles"
-    )
+class Topic(Base):
+    __tablename__ = "topic"
 
-class 
+    id = Column(Integer, primary_key=True)
+    topic_str = Column(String(100), nullable=False)
 
+class ArticleToTopic(Base):
+    __tablename__ = "article_to_topic"
+    
+    article_id = Column(Integer, ForeignKey("article.id"), primary_key=True)
+    topic_id = Column(Integer, ForeginKey("topic.id"), primary_key=True)
+    weight = Column(Float, nullable=False)
+
+    article = relationship("Article", backref="article_topics")
+    topic = relationship("Topic", backref="topic_articles")
+
+engine = create_engine("sqlite:///newsie_data.db")
+
+Base.metadata.create_all(engine)
